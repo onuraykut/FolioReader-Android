9872350ce618ab4fb14277abac7c775a0f3e4c8c
Onur Aykut <onuraykut@outlook.com>
2021-11-15 15:13:26 +0300

    tr app  yeni(15.11.2021)ingiliz admob after font fix

--- sample/src/main/java/com/folioreader/android/sample/HomeActivity.java
+++ sample/src/main/java/com/folioreader/android/sample/HomeActivity.java
@@ -97,6 +97,7 @@ public class HomeActivity extends AppCompatActivity
                     config = new Config();
                     config.setAllowedDirection(Config.AllowedDirection.VERTICAL_AND_HORIZONTAL);
                     config.setBackgroundColorInt(2);
+                    config.setFont(4);
 
                 }
                 config.setPremium(false);
@@ -119,7 +120,7 @@ public class HomeActivity extends AppCompatActivity
                     folioReader.setReadLocator(readLocator);
                 }
                 folioReader.setConfig(config, true)
-                        .openBook("file:///android_asset/TheSilverChair.epub");
+                        .openBook("file:///android_asset/dusunce.epub");
             }
         });
     }
